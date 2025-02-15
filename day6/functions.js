a3bc94ae321273ef0fd38c0bffff59adab7a12cf
Shajitha <Shajitha090204@gmail.com>
2025-02-15 18:27:00 +0530

    added 4 types of function

---
+++ day6/functions.js
@@ -0,0 +1,21 @@
+function noParameterNoReturn(){
+    console.log("This is my function");
+}
+noParameterNoReturn();
+
+function noParameterHasReturn(){
+    return("This is my function1");
+}
+console.log(noParameterHasReturn());
+
+function hasParameterNoReturn(name){
+    console.log(`Hello, ${name} !`);
+}
+//use backticks ` dont use " " or '' while using string interpolation
+hasParameterNoReturn("Shajitha");
+
+function hasParameterhasReturn(name){
+    return(`Hello, ${name} !,Welcome back.`);
+}
+//use backticks ` dont use " " or '' while using string interpolation
+console.log(hasParameterhasReturn("Shajitha"));
