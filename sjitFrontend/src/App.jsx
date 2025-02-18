66f832f4070efbf6a5bbf1819eb2a17c716d547d
Shajitha <Shajitha090204@gmail.com>
2025-02-18 21:12:11 +0530

    react basics

---
+++ sjitFrontend/src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
+import Home from "./components/FunctionalComponent/Home"
+
+function App() {
+  /*const [count, setCount] = useState(0)*/
+
+  return ( 
+    <>
+    <div>
+      <h1>Welcome to React</h1>
+      <hr />
+      <Home></Home>
+    </div>
+    </>
+  );
+}
+
+export default App;
