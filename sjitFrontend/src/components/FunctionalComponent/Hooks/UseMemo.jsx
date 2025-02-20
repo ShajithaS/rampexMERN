a84b91c72cc2b2af120c2ca0fad721720279b86b
Shajitha <Shajitha090204@gmail.com>
2025-02-20 20:45:22 +0530

    debug slowFunction

--- sjitFrontend/src/components/FunctionalComponent/Hooks/UseMemo.jsx
+++ sjitFrontend/src/components/FunctionalComponent/Hooks/UseMemo.jsx
@@ -4,9 +4,9 @@ function doublingUpNumber(num){
     return num*2;
 }
 function slowFunction(num){
-    for(var i=0;i<1000;i++){
-        return num*2
+    for(var i=0;i<100000000000;i++){
     }
+    return num*2
 }
 const UseMemo =()=>{
     var[number,setNumber]=useState(0);
@@ -21,7 +21,7 @@ const UseMemo =()=>{
             <h1>This ist the example of useMemo</h1>
             Number box: {""}
             <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
-            <h2>The number is {number}</h2>
+            <h2>The number  is {number}</h2>
             <h2>the number is {slowFunction(number)}</h2>
             <button onClick={()=>setTheme(!theme)}>Toggle theme</button>
         </div>
