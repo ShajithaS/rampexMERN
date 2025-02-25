766f7ff3d0a73d9008a7b806396d5cb0d0d2aad1
Shajitha <Shajitha090204@gmail.com>
2025-02-25 14:42:22 +0530

    Hashing password

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const mdb = require("mongoose");
 const dotenv = require("dotenv");
+const bcrypt=require("bcrypt")
 dotenv.config();
 const Signup = require("./models/signupSchema");
 const app = express(); /* responsible for creating server */
 app.use(express.json()) 
 const PORT = 3001;
-
-
 
 console.log(process.env.MONGODB_URL);
 
@@ -20,31 +19,37 @@ mdb
     console.log("Check your connection string", err);
   });
 /**/
+
 app.get("/", (req, res) => {
   res.send("<h1>Welcome to Backend</h1>");
 });
+
 app.get("/static", (req, res) => {
   res.sendFile("/home/shaji/MERN_TRAINING/index.html");
 });
-app.post("/signup", (req, res) => {
+
+app.post("/signup", async(req, res) => {
 try {
+
+    
     console.log(req.body)
     const {firstname,lastname,phoneNumber,password,email} = req.body
+    const hashedpassword=await bcrypt.hash(password,10)
     const newSignup = new Signup({
     firstname: firstname,
     lastname: lastname,
     phoneNumber: phoneNumber,
-    password: password,
+    password: hashedpassword,
     email: email
   });
  
     newSignup.save();
     console.log("Signup succesful")
-    res.status(201).json({message:"Signup succesful",isSignup:true})
+    res.status(201).json({message:"Signup successful",isSignup:true})
   } 
     catch (error) {
     console.log(error)
-    res.status(201).json({message:"Signup unsuccesful",isSignup:false})
+    res.status(400).json({message:"Signup unsuccesful",isSignup:false})
   }
 });
 
